e767572d3a50a36d9d1cbec7d0f59cc2c85bd67c
Ben Sooraj <bensoorajm@gmail.com>
2018-05-03 19:33:59 +0530

    Google Service account and loaded keys to GOOGLE_APPLICATION_CREDENTIALS

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,7 @@ router.get('/', function (req, res, next) {
 /* POST to respond to sendImageDataUrlToServer call from the browser */
 router.post('/receiveDataUrl', function (req, res, next) {
   console.log(req.body.base64Image.substring(0, 50));
+  console.log(JSON.stringify(process.env.GOOGLE_APPLICATION_CREDENTIALS));
   res.json({
     hello: req.body.base64Image.substring(0, 50)
   });
