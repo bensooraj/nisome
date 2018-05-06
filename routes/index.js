a184cef74a0db687bb441267cb381cedf0201e82
Ben Sooraj <bensoorajm@gmail.com>
2018-05-06 06:47:14 +0530

    MVP 0.1 Ready

--- routes/index.js
+++ routes/index.js
@@ -34,70 +34,47 @@ router.post('/receiveDataUrl', function (req, res, next) {
     console.log(Buffer.isBuffer(imageBuffer));
 
     // Performs label detection on the image file
-    // client
-    //     .textDetection({
-    //         image: {
-    //             content: imageBuffer
-    //         }
-    //     }).then(response => {
-    //         const content = response[0].textAnnotations;
-    //         console.log('Labels:');
-    //         content.forEach(text => console.log(text.description));
-    //         const fullText = response[0].fullTextAnnotation.text;
-    //         // Strip new lines and/or carriage returns
-
-    //         // Define the params for making the text to speech request
-    //         var params = {
-    //             text: "Hi", //fullText.replace(/\r?\n|\r/g, ""),
-    //             voice: 'en-US_AllisonVoice',
-    //             accept: 'audio/wav'
-    //         };
-
-    //         // Call the text to speech API
-    //         textToSpeech
-    //             .synthesize(params, function (err, audio) {
-    //                 if (err) {
-    //                     console.log(err);
-    //                     throw "There was an error with the IBM Text to Speech API: " + err.toString();
-    //                 }
-    //                 textToSpeech.repairWavHeader(audio);
-
-    //                 return res.json({
-    //                     error: null,
-    //                     fullText,
-    //                     audio
-    //                 });
-
-    //             });
-
-    //     }).catch(err => {
-    //         console.error(err);
-    //         return res.json({
-    //             error: err.toString()
-    //         });
-    //     });
-    // Define the params for making the text to speech request
-    var params = {
-        text: "Ben",
-        voice: 'en-US_AllisonVoice',
-        accept: 'audio/wav'
-    };
-
-    // Call the text to speech API
-    textToSpeech
-        .synthesize(params, function (err, audio) {
-            if (err) {
-                console.log(err);
-                throw "There was an error with the IBM Text to Speech API: " + err.toString();
+    client
+        .textDetection({
+            image: {
+                content: imageBuffer
             }
-            textToSpeech.repairWavHeader(audio);
+        }).then(response => {
+            const content = response[0].textAnnotations;
+            console.log('Labels:');
+            content.forEach(text => console.log(text.description));
+            const fullText = response[0].fullTextAnnotation.text;
+            // Strip new lines and/or carriage returns
 
+            // Define the params for making the text to speech request
+            var params = {
+                text: fullText.replace(/\r?\n|\r/g, ""),
+                voice: 'en-US_AllisonVoice',
+                accept: 'audio/wav'
+            };
+
+            // Call the text to speech API
+            textToSpeech
+                .synthesize(params, function (err, audio) {
+                    if (err) {
+                        console.log(err);
+                        throw "There was an error with the IBM Text to Speech API: " + err.toString();
+                    }
+                    textToSpeech.repairWavHeader(audio);
+
+                    return res.json({
+                        error: null,
+                        fullText,
+                        audio
+                    });
+
+                });
+
+        }).catch(err => {
+            console.error(err);
             return res.json({
-                error: null,
-                fullText: "fullText",
-                audio
+                error: err.toString()
             });
-
         });
 });
 
